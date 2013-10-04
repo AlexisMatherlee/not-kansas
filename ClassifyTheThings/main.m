0663ae947c583779211f34c6bff51b0299d454c3
Alexis Matherlee <alexismatherlee@gmail.com>
2013-10-03 19:56:50 -0700

    Initial Commit

---
+++ ClassifyTheThings/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ClassifyTheThings
+//
+//  Created by Alexis Matherlee on 10/3/13.
+//  Copyright (c) 2013 Alexis Matherlee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
